93926b55cbc7863d9593a83a8f057af0844304c7
Egan Nakano <enakano@hawaii.edu>
2018-04-26 20:55:19 -1000

    Added faqs html and js

---
+++ app/imports/ui/pages/faqs.js
@@ -0,0 +1,8 @@
+$('.ui.accordion')
+  .accordion()
+;
+
+function backToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
