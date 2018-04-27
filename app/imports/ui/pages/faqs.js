f7bcf7cde2187955448dbc9fdf89167193866ca8
Egan Nakano <enakano@hawaii.edu>
2018-04-26 21:48:09 -1000

    reverting test

--- app/imports/ui/pages/faqs.js
+++ app/imports/ui/pages/faqs.js
@@ -5,3 +5,8 @@ import { Template } from 'meteor/templating';
 $('.ui.accordion')
   .accordion('refresh')
 ;
+
+function backToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
